691608b64fad25c8dad3663799153abd872dea88
ilhamtrinanda <ilhmtrnnda@gmail.com>
2025-10-01 23:19:14 +0700

    membuat landing page dan login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,33 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+// Landing page
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
 });
+
+// Cara pemesanan page
+Route::get('/cara-pemesanan', function () {
+    return view('cara-pemesanan');
+});
+
+// Login page (Breeze)
+Route::get('/login-page', function () {
+    return view('auth.login');
+})->name('login.page');
+
+// Dashboard (Breeze)
+Route::get('/dashboard', function () {
+    return view('dashboard'); // gunakan x-app-layout
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+// Profile routes (Breeze)
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
